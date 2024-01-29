2e25f61823388869460f889ebcb670ad40d87c93
Yadavendra Sakharkar <yadavendra@Yadavendras-MacBook-Air.local>
2024-01-30 03:00:05 +0530

    adding todolist

---
+++ TODO-list/todos.js
@@ -0,0 +1,23 @@
+let input = prompt("What would you like to do?");
+let todos = ["buy eggs", "buy bread"];
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("*******");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i} : ${todos[i]}`);
+    }
+    console.log("*******");
+  } else if (input === "new") {
+    let newTodo = prompt("What is the new todo you want to add?");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to the list`);
+  } else if (input === "delete") {
+    let index = parseInt(
+      prompt("Ok, Enter the index of the todo you want to delete")
+    );
+    const deleted = todos.splice(index, 1);
+    console.log(`Deleted todo: ${deleted}`);
+  }
+  input = prompt("What would you like to b?");
+}
+console.log("You quit the app");
